bda875768de87ec706b6c1056e68b4cb6992ce52
Hussain Ahmed <Hussain.ahmed@city.ac.uk>
2017-07-18 10:14:28 +0100

    Files added

---
+++ Exercise 2/Cinema booking system/Cinemabookingsystem/src/Student.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+        String y=(String)jComboBox1.getSelectedItem();
+        int b = Integer.parseInt(y);
+        int r=8*b;
+        String z=Integer.toString(r);
+        jTextField1.setText(z);
+ */
+
+/**
+ *
+ * @author akboir
+ */
+public class Student {
+    
+    //Student normal price
+    
+}
